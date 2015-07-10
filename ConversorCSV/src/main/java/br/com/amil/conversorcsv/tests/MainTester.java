68adfc7e81d32373851b306b412a9e776268cfc5
fsilvapucci@gmail.com <fsilvapucci@gmail.com>
2015-07-10 19:28:40 -0300

    Adição dos arquivos jasper

--- ConversorCSV/src/main/java/br/com/amil/conversorcsv/tests/MainTester.java
+++ ConversorCSV/src/main/java/br/com/amil/conversorcsv/tests/MainTester.java
@@ -6,6 +6,12 @@ import br.com.amil.conversorcsv.view.ListaDadosView;
 
 import java.io.File;
 import java.util.List;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 
 public class MainTester {
@@ -16,7 +22,8 @@ public class MainTester {
         try {
            
              List<Relatorio> relatorios = RelatorioUtil.carregarListaRelatorios(new File("/home/fernando-pucci/ConversorCSV/CSV/720023_145_Amil 120 CE.csv"));
-                 
+            MainTester mc = new MainTester();     
+            mc.imprimir(relatorios);
 //             for(Relatorio r : relatorios){
 //             
 //                 System.out.println(r.toString());
@@ -35,5 +42,20 @@ public class MainTester {
         }
 
     }
+    
+    	public void imprimir(List<Relatorio> clientes) throws Exception	
+	{
+            
+
+            String caminho =this.getClass().getClassLoader().getResource("").getPath(); 
+          
+            
+            System.out.println(System.getProperty("user.dir"));
+		JasperReport report = JasperCompileManager.compileReport(System.getProperty("user.dir")+"/src/main/java/br/com/amil/conversorcsv/reports/templatePlt2.jrxml");
+		
+		JasperPrint print = JasperFillManager.fillReport(report, null, new JRBeanCollectionDataSource(clientes));
+ 
+		JasperExportManager.exportReportToPdfFile(print, "/home/fernandopucci/Relatorio_de_Clientes.pdf");		
+	}
   
 }
